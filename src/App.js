319a5527f65f0431abd210e81a95a0d669726ff8
cowboysj <su10jin11@khu.ac.kr>
2023-10-04 19:47:09 +0900

    3주차 실습, 미션

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Week1 from "./Week1";
-import Week2 from "./Week2";
+import React from "react";
+import Movie from "./Components/Movie";
+import { movies } from "./movieDummy";
+import "./css/Movie.css";
 
 function App() {
-  return (
-    <div>
-      <Week2 />
-    </div>
-  );
+  const moviesPerPage = 2;
+  const movieRows = [];
+  console.log(movies.results.length);
+
+  for (let i = 0; i < movies.results.length; i += moviesPerPage) {
+    const rowMovies = movies.results.slice(i, i + moviesPerPage);
+    console.log(rowMovies);
+    const movieRow = (
+      <div className="row2" key={`row-${i}`}>
+        {rowMovies.map((item) => (
+          <Movie key={item.id} data={item} />
+        ))}
+      </div>
+    );
+
+    movieRows.push(movieRow);
+  }
+
+  return <div className="app-container">{movieRows}</div>;
 }
 
 export default App;
